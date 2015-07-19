9da824672906743e70e43297f53329a1192acf46
surreptakos <djgatsakos@gmail.com>
2015-07-18 21:35:00 -0500

    a

---
+++ com/caveofprogramming/designpatterns/demo1/model/Person.java
@@ -0,0 +1,37 @@
+package com.caveofprogramming.designpatterns.demo1.model;
+
+public class Person {
+	private int id;
+	private String name;
+	private String password;
+	
+	public Person() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
